9b6c3a9e30bb59ce6555eef82f6e6aaf89407587
Rajneesh kumar <rajnishkumar550443@gmail.com>
2024-01-18 15:55:43 +0530

    Sweet alert

--- javaScr/javaScr.js
+++ javaScr/javaScr.js
@@ -9,6 +9,6 @@ function sentEmail() {
              +"<br> Phone: " + document.getElementById("phone").value
              +"<br> Message: " + document.getElementById("message").value
     }).then(
-        message => alert("message send")
+        message => swal("Success", "We will contact you soon.")
     );
 };
